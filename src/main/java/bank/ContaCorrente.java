6d37fbfe41c4a3cd4d1ef32daf4a52f34ffb98b6
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-28 22:40:24 -0300

    Composição com herança

---
+++ src/main/java/bank/ContaCorrente.java
@@ -0,0 +1,28 @@
+package bank;
+
+public class ContaCorrente implements Conta {
+
+    private double saldo;
+
+    @Override
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+
+    }
+
+    @Override
+    public void transferir(Conta conta, double valor) {
+        this.saldo -= valor;
+        conta.depositar(valor);
+    }
+}
