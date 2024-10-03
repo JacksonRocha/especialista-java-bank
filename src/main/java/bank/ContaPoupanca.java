24b84f88c41ab9f61a72c91051a765ab56ab795d
jacksonrocha <jackson_lbr@hotmail.com>
2024-10-02 21:55:17 -0300

    Composição com implements

---
+++ src/main/java/bank/ContaPoupanca.java
@@ -0,0 +1,37 @@
+package bank;
+
+public class ContaPoupanca implements Conta {
+
+    private double saldo;
+
+    @Override
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+
+    }
+
+    @Override
+    public void transferir(Conta conta, double valor) {
+        this.saldo -= valor;
+        conta.depositar(valor);
+    }
+
+    @Override
+    public void aplicarEmInvestimento(double valor) {
+        this.saldo -= valor;
+    }
+
+    public void sacarTarifa(double valorTarifa) {
+        this.saldo -= valorTarifa;
+    }
+}
